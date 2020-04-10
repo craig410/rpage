71c7e74c9ba48321419044b80f1265fc2a43cd8d
craig410 <craig@netchaos.net>
2020-04-10 12:51:37 +0100

    Support bootstrap4

--- responsive-paginate.js
+++ responsive-paginate.js
@@ -69,7 +69,7 @@
         			return false;
         		}
         		var index = this.els.filter(element).index();
-        		if (index == 1 || this.isNextOrPrevLink($container.find("li").eq(index + 1)))
+        		if (index == 1 || this.isNextOrPrevLink($container.children("li").eq(index + 1)))
         		{
         			return false;
         		}
@@ -83,7 +83,7 @@
     	    this.removeOne = function()
     	    {
     	    	var active_index = this.active_index;
-    	    	var farthest_index = $container.find("li").length - 1;
+    	    	var farthest_index = $container.children("li").length - 1;
     	    	// var next = active_index + 1;
     	    	// var prev = active_index - 1;
 
@@ -91,7 +91,7 @@
     	    	{
     	    		var candidates = this.els.filter(".page-away-" + i.toString());
     	    		var candidate = candidates.filter(function(){
-    	    			return this.style["display"] != "none";
+    	    			return ! ($(this).hasClass("d-none"));
     	    		});
     	    		if (candidate.length > 0)
     	    		{
@@ -100,20 +100,20 @@
     	    				var candid_candidate = candidate.eq(j);
     	    				if (this.isRemovable(candid_candidate))
     		    			{
-    			    			candid_candidate.css("display", "none");
-    								var rpageindex = candid_candidate.data("rpage-index");
+										candid_candidate.addClass("d-none");
+										var rpageindex = candid_candidate.data("rpage-index");
     			    			if (this.needsEtcSign(active_index, farthest_index - 1))
     			    			{
-    			    				this.els.eq(farthest_index - 2).before("<li class='disabled removable'><span>...</span></li>");
+    			    				this.els.eq(farthest_index - 2).before("<li class='page-item disabled removable'><span class='page-link'>...</span></li>");
     			    			}
     			    			else if (this.needsEtcSign(1, active_index))
     			    			{
-    			    				this.els.eq(1).after("<li class='disabled removable'><span>...</span></li>");
-    			    			}
+    			    				this.els.eq(1).after("<li class='page-item disabled removable'><span class='page-link'>...</span></li>");
+										}
     			    			else
     			    			{
-    			    			  this.elements[rpageindex].visible = false;
-    			    			}
+											this.elements[rpageindex].visible = false;
+										}
 
     			    			return true;
     		    			}
@@ -135,8 +135,8 @@
     	    		var hasHiddenElement = false;
     	    		for (var i = el1_index + 1; i < el2_index; i++)
     	    		{
-    	    			var el = $container.find("li").eq(i);
-    	    			if (el.css("display") == "none")
+    	    			var el = $container.children("li").eq(i);
+    	    			if (el.hasClass("d-none"))
     	    			{
     	    				hasHiddenElement = true;
     	    			}
@@ -157,55 +157,55 @@
     	    {
     	    	for (var i = 0; i < this.els.length; i++)
     	    	{
-    	    		this.els.eq(i).css("display", "inline");
+    	    		this.els.eq(i).removeClass("d-none");
     	    	}
-    	    	$container.find("li").filter(".removable").remove();
+    	    	$container.children("li").filter(".removable").remove();
 
-    	    	element_widths = 0;
-    	    	this.elements = Array();
+						element_widths = 0;
+						this.elements = Array();
     	    }
 
             this.calculateWidth = function()
             {
-            	if (element_widths !== 1){
-            		element_widths = 1;
+							if (element_widths !== 1){
+								element_widths = 1;
                 var width = 0;
                 for (var i = 0; i < this.els.length; i++)
                 {
-                	var $el = $(this.els[i]);
-                	var elwidth = 0;
-                    if(!($el.css('display') === 'none'))
+									var $el = $(this.els[i]);
+									var elwidth = 0;
+                    if(!($el.hasClass("d-none")))
                     {
                         if($el.children("a").eq(0).length > 0){
-                        	elwidth += $el.children("a").eq(0).outerWidth();
+													elwidth += $el.children("a").eq(0).outerWidth();
                         }
                         if($el.children("span").eq(0).length > 0){
-                        	elwidth += $el.children("span").eq(0).outerWidth();
+													elwidth += $el.children("span").eq(0).outerWidth();
                         }
                     }
-                	width += elwidth;
-                	$el.data("rpage-index",i);
-                	this.elements[i] = {"width":elwidth, "visible":true};
+									width += elwidth;
+									$el.data("rpage-index",i);
+									this.elements[i] = {"width":elwidth, "visible":true};
                 }
                 return width;
-              } else {
-              	width = 0;
-              	for (i = 0; i < this.elements.length; ++i) {
-              		if (this.elements[i].visible === true){
-              			width += this.elements[i].width;
-              		}
-              	}
-              	return width;
-              }
+							} else {
+								width = 0;
+								for (i = 0; i < this.elements.length; ++i) {
+									if (this.elements[i].visible === true){
+										width += this.elements[i].width;
+									}
+								}
+								return width;
+							}
             }
 
-    	    this.els = $container.find("li");
+    	    this.els = $container.children("li");
 
-    	    var element_widths = 0;
-    	    this.elements = Array();
-    	    this.active_index = this.els.filter(".active").index();
+					var element_widths = 0;
+					this.elements = Array();
+					this.active_index = this.els.filter(".active").index();
 
-    	    this.label();
+					this.label();
     	    this.makeResponsive();
 
     	    var resize_timer;
